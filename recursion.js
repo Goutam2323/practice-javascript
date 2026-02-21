a666f0e9e128f1f67aa61e495ea54e15c1f701cf
Goutam2323 <bishtgoutam3@gmail.com>
2026-02-21 12:37:43 +0530

    correceted recursion

--- recursion.js
+++ recursion.js
@@ -2,6 +2,14 @@ function fibonaaci(n){
     if(n <= 1){
         return n;
     }
-    return fibonaaci(n-1) + fibonaaci(n-2);
+     return fibonaaci(n-1) + fibonaaci(n-2);
 }
-console.log(fibonaaci(11));
+function printFibonacce(n){
+for(i=1;i<=n;i++){
+    
+process.stdout.write(" " + fibonaaci(i).toString() );
+}
+console.log()
+}
+
+printFibonacce(11)
